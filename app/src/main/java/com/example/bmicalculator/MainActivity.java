1655c143157d9c4fef5ad74b1d7954155ed55b3d
bvekxettri <bvekxettri@gmail.com>
2019-10-21 13:39:45 +0545

    Validation done

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView txtRes;
@@ -25,28 +26,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btncal.setOnClickListener((View.OnClickListener) this);
     }
 
+    public boolean valid(){
+        if(et1.getText().toString().trim().equals("") || et2.getText().toString().trim().equals("")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     @Override
     public void onClick(View v) {
+
 
         String weight = et1.getText().toString();
         String height = et2.getText().toString();
 
-        float w = Float.parseFloat(weight);
-        float h =Float.parseFloat(height);
+        if(!valid()) {
 
-        float bmi = w / (h * h);
 
-        String category;
-        if (bmi < 18f) {
-            category = " underweight";
-        } else if (bmi >=18f && bmi <= 25f) {
-            category = "Normal";
+            float w = Float.parseFloat(weight);
+            float h = Float.parseFloat(height);
 
-        } else {
-            category ="Overweight";
+            float bmi = w / (h * h);
+
+            String category;
+            if (bmi < 18f) {
+                category = " underweight";
+            } else if (bmi >= 18f && bmi <= 25f) {
+                category = "Normal";
+
+            } else {
+                category = "Overweight";
+            }
+            category = bmi + "\n\n" + category;
+            txtRes.setText("BMI=" + category);
         }
-        category = bmi + "\n\n" + category;
-        txtRes.setText("BMI="+category);
-
+        else
+        {
+            Toast.makeText(this,"Please fill all the fields", Toast.LENGTH_LONG).show();
+        }
     }
 }
